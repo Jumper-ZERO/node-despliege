ef732df92f2d92ee520615b56217a0ec39a11737
Jeremi Aron <chancanjeremiaron@gmail.com>
2022-10-02 11:30:46 -0500

    express dev

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (res, req) => res.send("Hello World"));
+app.get("/", (res, req) => res.send("Hola Mundo"));
 
 app.listen(PORT, () => {
   console.log(`El puerto se esta iniciando en el puerto ${PORT}`);
